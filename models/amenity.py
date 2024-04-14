69c1255a96eb60e24a9a334d4eaa30a74f3476d8
root <root@X7T77.localdomain>
2024-04-14 07:57:29 -0500

    fix sqlalchemy mistake

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
 
-__tablename__ = "amenities"
-name = Column(String(128), nullable=False)
-place_amenities = relationship("Place",
-                                   secondary="place_amenity",
+class Amenity(BaseModel, Base):
+    """
+    Represents an Amenity for a MySQL database.
+    """
+
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
                                    viewonly=False)
--- models/place.py
+++ models/place.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import Base
 from models.review import Review
+from os import getenv
 
 table_associacion = Table("place_amenity", Base.metadata,
                           Column(
