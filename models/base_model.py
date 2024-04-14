23dd4a71868574bda03fd40607bbf2eabf7ee732
root <root@X7T77.localdomain>
2024-04-14 08:19:52 -0500

    fix sqlalchemy mistake

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines the BaseModel class."""
 import models
-from uuid import uuid4
+import uuid
 from datetime import datetime, timezone
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column
