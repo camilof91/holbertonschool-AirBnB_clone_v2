3ca53dfb2206e72f1d4198442f55b1b5c7bfcfce
camilof91 <camilofetecua2011@outlook.com>
2024-04-23 19:57:17 -0500

    add py rute

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    This function handles the root URL (/) and returns "Hello HBNB".
+    """
+    return "Hello HBNB"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    This function handles the URL /hbnb and returns "HBNB".
+    """
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_rute(text):
+    """
+    This function handles URLs like /c/something, where <text> captures the value
+    after "/c/". It replaces underscores with spaces and returns "C " followed by
+    the modified text.
+    """
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py_rute(text=None):
+    """
+    This function handles URLs like /c/something, where <text> captures the value
+    after "/python/". It replaces underscores with spaces and returns "C " followed by
+    the modified text.
+    """
+    if text is None:
+        text = "is cool"
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
