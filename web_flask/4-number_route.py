5c93d3d735193191ea93fcd2b8b85b81afc3fdad
camilof91 <camilofetecua2011@outlook.com>
2024-04-24 10:24:16 -0500

    add num_rute

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -41,5 +41,15 @@ def py_rute(text=None):
     text = text.replace("_", " ")
     return f"Python {text}"
 
+@app.route("/number/<n>", strict_slashes=False)
+def num_rute(n):
+    """
+    This function handles URLs like /c/something, where <text> captures the value
+    after "/python/". It replaces underscores with spaces and returns "C " followed by
+    the modified text.
+    """
+    number = int(n)
+    return f"{number} is a number"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
